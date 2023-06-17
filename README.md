715b2571eda2e83262810989278280c8664becb9
Roman Pravda <roman.p@knowledgecity.pro>
2023-06-17 18:25:36 +0400

    The Big Bang

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # invaders
  An open-sourced version of the arcade game "Space Invaders"
+
+ Audio has taken from [https://github.com/CleanCut/invaders] repository.
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "audio/explode.wav");
+    audio.add("lose", "audio/lose.wav");
+    audio.add("move", "audio/move.wav");
+    audio.add("pew", "audio/pew.wav");
+    audio.add("startup", "audio/startup.wav");
+    audio.add("win", "audio/win.wav");
+    
+    // Start-up
+    audio.play("startup");
+    
+    // Exiting
+    audio.wait();
+    Ok(())
